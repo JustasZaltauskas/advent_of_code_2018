1e01b91b005e7701383030d0a6ee319c7a9eff05
Justas Žaltauskas <zaltauskasjustas@gmail.com>
2019-01-12 17:41:15 +0200

    Refactor 4th task part 1

--- AOC/4/index.ts
+++ AOC/4/index.ts
@@ -20,71 +20,41 @@ interface ISleepData {
 const findSleepyGuard = (input: string[][]) => {
     input.sort();
 
-    const { sleepData } = input.reduce((acc, sleep) => {
-        const [timestamp, action] = sleep;
-        const [_, time] = timestamp.split(' ');
+    let guardId: string;
+    let asleepMinutes: number;
+    let sleepData: ISleepData = {};
+
+    input.forEach(([timestamp, action]) => {
+        const currentMinutes = Number(timestamp.split(' ')[1].split(':')[1]);
         const number = action.match(/\d+/);
 
-        if (number) {
-            return {
-                ...acc,
-                guardId: number[0],
-            }
-        }
-
-        const currentMinutes = Number(time.split(':')[1]);
-        if (action.search(/falls/) !== -1) {
-            return {
-                ...acc,
-                asleepTime: currentMinutes,
-            }
-        };
-
-        if (action.search(/wakes/) !== -1) {
-            const id = acc.guardId;
-            const sleepData = acc.sleepData[id] || {
+        if (guardId && !sleepData.hasOwnProperty(guardId)) {
+            sleepData[guardId] = {
                 sleepTime: 0,
                 minutes: Array(60).fill(0),
-            };
-            const asleepTime = acc.asleepTime || 0;
-            const sleepTime = sleepData.sleepTime;
-
-            const newMinutes = sleepData.minutes
-                .slice(asleepTime, currentMinutes)
-                .map(min => ++min);
-
-            sleepData.minutes.splice(
-                asleepTime,
-                newMinutes.length,
-                ...newMinutes
-            );
-
-            return {
-                ...acc,
-                sleepData: {
-                    ...acc.sleepData,
-                    [id]: {
-                        sleepTime: sleepTime + (currentMinutes - asleepTime - 1),
-                        minutes: sleepData.minutes,
-                    }
-                }
             }
         }
 
-        return acc;
-    }, {
-        sleepData: {}
-    } as {
-        sleepData: ISleepData,
-        guardId: string,
-        asleepTime: number,
-    });
+        if (number) {
+            guardId = number[0];
+        }
 
+        if (action.search(/falls/) !== -1) {
+            asleepMinutes = currentMinutes;
+        }
+
+        if (action.search(/wakes/) !== -1) {
+            for (let i = asleepMinutes; i < currentMinutes; i++) {
+                sleepData[guardId].minutes[i]++;
+            }
+
+            sleepData[guardId].sleepTime += currentMinutes - asleepMinutes - 1;
+        }
+    });
 
     const [sleepyGuardId, sleepyGuard] = maxEntry(
         sleepData, (x: ISleepData) => x.sleepTime);
     const [maxMinute] = maxEntry(sleepyGuard.minutes);
-
 
     return Number(sleepyGuardId) * Number(maxMinute);
 }
@@ -92,5 +62,4 @@ const findSleepyGuard = (input: string[][]) => {
 const maxEntry = (o: object, getValue: Function = identity) => Object.entries(o)
     .reduce((acc, x) => getValue(acc[1]) < getValue(x[1]) ? x : acc);
 
-const x = findSleepyGuard(input)
-console.log(x);
+console.log(findSleepyGuard(input));
