0cd1c1b3c885e3d8c2c40db174e0c1c5f10b1460
Justas Žaltauskas <zaltauskasjustas@gmail.com>
2019-01-13 15:04:03 +0200

    Add 5th task part 1 stack implementation

--- AOC/5/index.ts
+++ AOC/5/index.ts
@@ -10,6 +10,10 @@ const input: string = readInput(path.resolve(__dirname, 'input'));
 const isUppercase = (code: number) => code >= 65 && code <= 90;
 const isLowercase = (code: number) => code >= 97 && code <= 122;
 
+const isPolymer = (x: string, y: string): boolean =>
+    x.toLowerCase() === y.toLowerCase() &&
+    isLowercase(y.charCodeAt(0)) !== isLowercase(x.charCodeAt(0));
+
 const reactPolymer = (s: string): number => {
     for (let i = 0; i < s.length; i++) {
         const letter = s[i];
@@ -17,10 +21,7 @@ const reactPolymer = (s: string): number => {
 
         if (typeof letterBefore === 'undefined') continue;
 
-        if (letter.toLowerCase() === letterBefore.toLowerCase() &&
-            (isLowercase(letterBefore.charCodeAt(0)) !==
-                isLowercase(letter.charCodeAt(0)))
-        ) {
+        if (isPolymer(letterBefore, letter)) {
             s = s.slice(0, i - 1) + s.slice(i + 1);
             i -= 2;
         }
@@ -29,13 +30,28 @@ const reactPolymer = (s: string): number => {
     return s.length;
 };
 
+const peek = <T>(stack: T[]): T => stack[stack.length - 1];
+// Stack implementation
+const reactPolymerv2 = (s: string): number => {
+    const stack: string[] = [];
+
+    s.split('').forEach((char) => {
+        if (!stack.length || !isPolymer(peek(stack), char)) {
+            stack.push(char);
+        } else {
+            stack.pop();
+        }
+    });
+
+    return stack.length;
+}
 
 const findShortestPolymer = (s: string): number => {
-    const polymers: number[] = Array(26)
+    const polymers = Array(26)
         .fill(0)
         .map((_, i) => {
-            const letterToRemove = String.fromCharCode(i + 65);
-            const polymer = s.replace(new RegExp(letterToRemove, 'gi'), '');
+            const re = new RegExp(String.fromCharCode(i + 65), 'gi');
+            const polymer = s.replace(re, '');
             return reactPolymer(polymer);
         });
 
@@ -43,5 +59,6 @@ const findShortestPolymer = (s: string): number => {
     return Math.min(...polymers);
 }
 
-// console.log(scanPolymer(input[0]));
-console.log(findShortestPolymer(input[0]));
+console.log(reactPolymer(input[0]));
+// console.log(reactPolymerv2(input[0]));
+// console.log(findShortestPolymer(input[0]));
