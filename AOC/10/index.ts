d30318d6bdccb1104500b4385023913929594dbd
justaszaltauskas <zaltauskasjustas@gmail.com>
2019-01-30 21:16:56 +0200

    Add 10th task part 1 solution with limited window size

--- AOC/10/index.ts
+++ AOC/10/index.ts
@@ -4,30 +4,31 @@ import path from 'path';
 // position=<-41630, -31239> velocity=< 4,  3>
 type Move = [number, number, number , number];
 const input: Move[] = readInput(path.resolve(__dirname, 'input'))
-    .map((s: string) => (s.match(/[+|-]?\d+/g) || []).map(Number));
+    .map((s: string) => (s.match(/-?\d+/g) || []).map(Number));
 
 const move = (moves: Move[]): Move[] =>
     moves.map(([x, y, dx, dy]): Move => [x + dx, y + dy, dx, dy]);
 
-const visualise = (moves: Move[]) => {
+const visualise = (moves: Move[], gridSize: number = 100) => {
     const minX = Math.min(...moves.map((x) => x[0]));
     const maxX = Math.max(...moves.map((x) => x[0]));
     const minY = Math.min(...moves.map((x) => x[1]));
     const maxY = Math.max(...moves.map((x) => x[1]));
     let matrix = '';
-    
-    for (let y = minY; y <= maxY; y++) {
-        for (let x = minX; x <= maxX; x++) {
-            if (moves.some(([mx, my]) => mx === x && my === y)) {
-                matrix += '# ';
-            } else {
-                matrix += '. ';
-            }
-        }
-        matrix += '\n';
-    }
 
-    console.log(matrix);
+    if ( gridSize >= maxX - minX && gridSize >= maxY - minY) {
+        for (let y = minY; y <= maxY; y++) {
+            for (let x = minX; x <= maxX; x++) {
+                if (moves.some(([mx, my]) => mx === x && my === y)) {
+                    matrix += '#';
+                } else {
+                    matrix += '.';
+                }
+            }
+            matrix += '\n';
+        }
+        console.log(matrix);
+    }
 };
 
 const moveTo = (n: number, moves: Move[]) => {
@@ -37,4 +38,4 @@ const moveTo = (n: number, moves: Move[]) => {
     }
 }
 
-moveTo(10, input);
+moveTo(100000, input);
