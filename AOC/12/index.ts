d729a45ddb53f14ef3c9fcb7817963b4f17db901
justaszaltauskas <zaltauskasjustas@gmail.com>
2019-02-10 21:49:02 +0200

    Add task 12 part 2 solution

--- AOC/12/index.ts
+++ AOC/12/index.ts
@@ -28,31 +28,47 @@ const rules = input
         return (acc[rule] = result, acc);
     }, {} as Rules);
 
-const generations = 20;
+const generations = 2000;
 
 type GeneratePlants = (state: string, rules: Rules, generations: number) => void;
 const generatePlants: GeneratePlants = (state, rules, generations) => {
-    let startLen = state.length;
     state = state.slice();
+    let zero_idx = 0;
   
     for (let i = 0; i < generations; i++) {
       state = '..'.concat(state, '..');
       let plants = Array(state.length);
       const readPlants = '..'.concat(state, '..');
+      zero_idx += 2;
     
       for (let j = 0; j < state.length; j++) {
         const plant = readPlants.substr(j, 5);
         plants[j] = rules[plant] || '.'; 
       }
-  
-      state = plants.join('');
+      
+      let start = 0;
+      let end = plants.length - 1;
+
+      while (plants[start] === '.') {
+        start++;
+        zero_idx -= 1;
+      }
+
+      while (plants[end] === '.') {
+        end--;
+      }
+
+      state = plants.slice(start, end + 1).join('');
+      console.log(i + 1, zero_idx, start, end);
     }
-  
-    const zero_idx = (state.length - startLen) / 2;
-    const result = state
+    
+    // part 2
+    zero_idx = (-Number(50e9)) + 82;
+    
+    const result =  state
       .split('')
-      .reduce((acc, x, i) => acc + (x === '#' ? i - zero_idx : 0), 0);
-  
+      .reduce((acc, x, i) => x === '#' ? acc + (i - zero_idx) : acc, 0);
+
     console.log(result);
   }
 
