de12c88c379befe98b75de86d878ec2bac3dd2c8
justaszaltauskas <zaltauskasjustas@gmail.com>
2019-02-23 19:04:27 +0200

    Add 14th task part 2

--- AOC/14/index.js
+++ AOC/14/index.js
@@ -2,15 +2,36 @@ const fs = require('fs');
 const recipesLimit = fs.readFileSync('input', 'utf-8')
     .split('\n')
     .map(Number)[0];
-const recipesToCount = 10;
 // indices in the recipes array
 const recipes = [3, 7];
 let elves = [0, 1];
+const isPart1 = false;
 
-while (recipes.length < recipesLimit + 10) {
-    const recipe = elves.reduce((r, x) => r + recipes[x], 0);
-    recipe.toString().split('').forEach(r => recipes.push(Number(r)));
-    elves = elves.map(e => (((1 + recipes[e] + e) % recipes.length)));
-}
+if (isPart1) {
+    const recipesToCount = 10;
 
-console.log(recipes.slice(recipesToCount).join(''));
+    while (recipes.length < recipesLimit + recipesToCount) {
+        const recipe = elves.reduce((r, x) => r + recipes[x], 0);
+        recipe.toString().split('').forEach(r => recipes.push(Number(r)));
+        elves = elves.map(e => (((1 + recipes[e] + e) % recipes.length)));
+    }
+
+    console.log(recipes.slice(-recipesToCount).join(''));
+} else {
+    const recipiesLimitStr = recipesLimit.toString();
+    let found = false;
+
+    while (!found) {
+        const recipe = elves.reduce((r, x) => r + recipes[x], 0);
+    
+        recipe.toString().split('').forEach(r => {
+            if (recipes.slice(-recipiesLimitStr.length).join('') === recipiesLimitStr) {
+                found = true;
+                console.log(recipes.length - recipiesLimitStr.length);
+            }
+            recipes.push(Number(r));
+        });
+    
+        elves = elves.map(e => ((1 + recipes[e] + e) % recipes.length));
+    }
+}    
